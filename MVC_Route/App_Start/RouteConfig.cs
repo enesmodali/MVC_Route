abde1e91b991981116e654840c0426ecb228ddee
e-mod <e-mod@DESKTOP-E9LRMFA>
2021-11-12 15:28:52 +0300

    Add project files.

---
+++ MVC_Route/App_Start/RouteConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_Route
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+          
+
+
+            // Urunler/Detaylar/{CategoryName}/{CategoryID}/{ProductName}/{ProductID}
+            // ÖR: Urunler/Detaylar/Beverages/1/Chai/1 şeklinde bir URL yapısı görünecektir.    
+            routes.MapRoute(
+                name:"UrunRoute",
+                url:"Urunler/Detaylar/{CategoryName}/{CategoryID}/{ProductName}/{ProductID}",
+                defaults: new 
+                {
+                controller= "Product",
+                action="Details",
+                CategoryName=UrlParameter.Optional,
+                CategoryID=UrlParameter.Optional,
+                ProductName=UrlParameter.Optional,
+                ProductID=UrlParameter.Optional
+                }
+                );
+
+            //Örnek: hepsiburada.com/UrunAdi-p-StokKodu
+            //Details için: 
+            //ProductName: Chai
+            //ProductID: 1
+            routes.MapRoute(
+                name:"UrunDetayRoute",
+                url:"{ProductName}-p-{ProductID}",
+                defaults: new 
+                {
+                controller = "Product",
+                action= "Details",
+                ProductName=UrlParameter.Optional,
+                ProductID=UrlParameter.Optional
+
+                }
+
+                );
+
+
+
+
+
+            //Default en altta kalacak. Çünkü yukarıdaki route lardan birini karşılayamazsa bunu kullanacak.
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVC_Route/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using MVC_Route.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Route.Controllers
+{
+    
+    public class ProductController : Controller
+    {
+       private NorthwindEntities db = new NorthwindEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = db.Products.ToList();
+            return View(products);
+        }
+
+        
+        public ActionResult Details(string ProductName, int? ProductID)
+        {
+
+            return View(db.Products.Where(x=> x.ProductID == ProductID).FirstOrDefault());
+        }
+
+        public ActionResult Edit(string CategoryName, int CategoryID,string ProductName, int ProductID) 
+        {
+            if (ProductID==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = db.Products.Find(ProductID);
+            if (product==null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName",product.SupplierID);
+            return View(product);
+
+        }
+
+    }
+}
